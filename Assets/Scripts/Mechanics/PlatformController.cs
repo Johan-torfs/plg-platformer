78aa12d0f72ff2b8613d2316219c4cbd0c3a1707
Johan Torfs <j.t.johan.torfs@gmail.com>
2022-11-26 19:34:12 +0100

    added draw method for platforms based on the width and reworked the platforms

--- Assets/Scripts/Mechanics/PlatformController.cs
+++ Assets/Scripts/Mechanics/PlatformController.cs
@@ -9,8 +9,26 @@ namespace Platformer.Mechanics
         public PatrolPath path;
         public float maxSpeed = 2.0f;
         internal PatrolPath.Mover mover;
+        SpriteRenderer spriteRenderer;
+        BoxCollider2D collider2d;
+
+        public Sprite[] platformTiles;
+        public Sprite finalSprite;
+
+        public int width = 3;
+        public int tileResolution = 100; 
+        public int pixelsPerUnit = 300;
 
         Vector2 move;
+
+        void Awake() {
+            Draw();
+            spriteRenderer = GetComponent<SpriteRenderer>();
+            collider2d = GetComponent<BoxCollider2D>();
+
+            spriteRenderer.sprite = finalSprite;
+            collider2d.size = new Vector2(tileResolution * width / pixelsPerUnit, tileResolution * 0.8f / pixelsPerUnit);
+        }
 
         void Update()
         {
@@ -19,6 +37,64 @@ namespace Platformer.Mechanics
                 if (mover == null) mover = path.CreateMover(maxSpeed * 0.5f);
                 move.x = Mathf.Clamp(mover.PositionLocal.x - transform.position.x, -1, 1);
                 transform.Translate(move);
+            }
+        }
+
+        private void Draw() {
+            Resources.UnloadUnusedAssets();
+            if (finalSprite == null)
+            {
+                Texture2D newTexture = new Texture2D(tileResolution * width, tileResolution);
+
+                // Set all pixels to transparent
+                for (int x = 0; x < newTexture.width; x++) 
+                {
+                    for (int y = 0; y < newTexture.width; y++) 
+                    {
+                        newTexture.SetPixel(x, y, new Color(1, 1, 1, 0));
+                    }
+                }
+
+                // Start with offset 0 (leftmost tile) and increase with every drawn tile
+                int offset = 0;
+
+                // Paint left tile
+                for (int x = 0; x < tileResolution; x++) 
+                {
+                    for (int y = 0; y < tileResolution; y++) 
+                    {
+                        newTexture.SetPixel(x + offset * tileResolution, y, platformTiles[0].texture.GetPixel(x, y));
+                    }
+                }
+                offset++;
+
+                // Paint middle tiles
+                for (int i = 1; i < width - 1; i++) 
+                {
+                    for (int x = 0; x < tileResolution; x++) 
+                    {
+                        for (int y = 0; y < tileResolution; y++) 
+                        {
+                            newTexture.SetPixel(x + offset * tileResolution, y, platformTiles[1].texture.GetPixel(x, y));
+                        }
+                    }
+                    offset++;
+                }
+
+                // Paint right tile
+                for (int x = 0; x < tileResolution; x++) 
+                {
+                    for (int y = 0; y < tileResolution; y++) 
+                    {
+                        newTexture.SetPixel(x + offset * tileResolution, y, platformTiles[2].texture.GetPixel(x, y));
+                    }
+                }
+
+                // Apply texture
+                newTexture.Apply();
+                
+                // Set final sprite
+                finalSprite = Sprite.Create(newTexture, new Rect(0, 0, newTexture.width, newTexture.height), new Vector2(0.5f, 0.5f), pixelsPerUnit);
             }
         }
     }
